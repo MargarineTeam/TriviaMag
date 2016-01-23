2505b9b5b33b30164e77f68e8003bc72fad56ba6
zhenyaracheva <zhenia.racheva@gmail.com>
2016-01-23 18:18:37 +0200

    Ninject changes again

---
+++ TriviaMag.Web/Games/Create.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TriviaMag.Services.Contracts;
+
+namespace TriviaMag.Web.Games
+{
+    public partial class Create : System.Web.UI.Page
+    {
+        [Inject]
+        public IGameService GameService { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.GameService.GetAll();
+        }
+    }
+}

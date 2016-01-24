b2f0a88b65d919a0cdf9a05951f37d20cf732886
juvemar <martoo.atanasov@gmail.com>
2016-01-24 18:00:06 +0200

    Creating the playgame page.

---
+++ TriviaMag.Web/Games/Play.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Play.aspx.cs" Inherits="TriviaMag.Web.Games.Play" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:FormView ID="playGameView" runat="server" SelectMethod="GetGameData" ItemType="TriviaMag.Models.Game">
+        <ItemTemplate>
+            <h3>
+                <strong><%# Item.Creator.UserName %></strong> vs <strong><%# Item.Receiver.UserName %></strong>
+            </h3>
+        </ItemTemplate>
+    </asp:FormView>
+    <asp:Label ID="QuestionLabel" runat="server"></asp:Label>
+</asp:Content>
---
+++ TriviaMag.Web/Games/Play.aspx.cs
@@ -0,0 +1,40 @@
+﻿namespace TriviaMag.Web.Games
+{
+    using Services.Contracts;
+    using System;
+    using System.Linq;
+
+    using Ninject;
+
+    using TriviaMag.Models;
+    using System.Web.ModelBinding;
+    public partial class Play : System.Web.UI.Page
+    {
+        private Game currentGame;
+        private int currentGameId;
+
+        [Inject]
+        public IGameService games { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadFirstQuestion(currentGameId);
+        }
+
+        private void LoadFirstQuestion([QueryString("id")]int gameID)
+        {
+            this.QuestionLabel.Text = this.games.GetById(gameID).Questions.FirstOrDefault().Text;
+        }
+
+        private string NextQuestion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetGameData([QueryString("id")]int gameID)
+        {
+            currentGameId = gameID;
+            return this.games.GetById(gameID);
+        }
+    }
+}
